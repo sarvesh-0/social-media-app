033cfcc35670647ae3a6c94e3f301c3fe19a69a9
sarvesh-0 <sarveshteware651@gmail.com>
2024-12-27 21:49:41 +0530

    Linkin Backend

---
+++ LinkinBackend/src/main/java/com/LinkinBackend/LinkinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.LinkinBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkinBackendApplication.class, args);
+	}
+
+}
